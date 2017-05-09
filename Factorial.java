ecc412a2f0a1b5064cb1b7beb3d66803bac87b52
jaguirr10 <jaguirr7@students.kennesaw.edu>
2017-05-09 14:41:47 -0400

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class Factorial
+
+ {
+	
+   public static void main(String args[]){
+	   
+      //Scanner object reads in user input
+	   
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Enter the number:");
+      
+      //Store number that is  entered value in variable
+      
+      int num = scanner.nextInt();
+      
+      //Called the user defined function fact
+      int factorial = fact(num);
+      
+      System.out.println("Factorial of the number entered is: "+factorial);
+   }
+   static int fact(int n)
+   {
+       int output;
+       if(n<=1){
+         return 1;
+       }
+       //Recursion: The fact function calls itself
+       
+       output = fact(n-1)* n;
+       return output;
+   }
+}
